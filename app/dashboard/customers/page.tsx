eb8cabd2d1fc7c3fa7a84d482acc1f245945a949
Ivan Gil <igil.dev@gmail.com>
2024-01-25 08:04:22 -0500

    app v1 complete

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,9 @@
 function CustomersPage() {
   return <p>Costumers page</p>;
 }
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default CustomersPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'Acme is a dashboard project built with Next.js.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh/'),
+};
+
 export default function RootLayout({
   children,
 }: {
